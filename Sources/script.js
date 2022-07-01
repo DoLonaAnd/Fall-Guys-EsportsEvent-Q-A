9fd35503f9dd21d83ee3aff9a1286ba4024fc5a2
Lona <103243217+DoLonaAnd@users.noreply.github.com>
2022-07-02 01:59:48 +0900

    [2022/07/02 01:59] Customized CSS

--- Sources/script.js
+++ Sources/script.js
@@ -3,13 +3,15 @@ const qalist = [
         ["会場はどこですか？"],
         ["参加費などはありますか？"],
         ["いつ開催されますか？"],
-        ["チーム通話は何を使うんですか？"]
+        ["チーム通話は何を使うんですか？"],
+        ["参加取り消しはどうすればいいんですか？"]
     ],
     [
         ["近畿大学 Eキャンパス E館1階eスポーツアリーナです。"],
         ["完全無料です！"],
         ["8月8日の月曜日です！　14時開始です！"],
-        ["Discordです！"]
+        ["Discordです！"],
+        ["参加フォームを編集して、「参加しますか？」の質問を「いいえ」で答えてください。"]
     ]
 ];
 
@@ -17,28 +19,30 @@ const index = [
     [
         ["#index"],
         ["#QAlist"],
-        ["#form"]
+        ["#formjoin"]
+        ["#form"],
     ],
     [
         ["目次"],
         ["Q & A"],
+        ["参加フォーム"],
         ["質問フォーム"]
     ]
 ];
 
 //生成テンプレート
 function indextemplate(num){
-    let words = '<div class="index"'+num+'><a href='+index[0][num]+'head>'+index[1][num]+'</a></div>';
+    let words = '<div id = "index'+num+'" class="index whiteblock"><a class="linkwhite" href='+index[0][num]+'head>'+index[1][num]+'</a></div>';
     return words;
 }
 function QAindextemplate(num){
-    let words = '<div id = "QAlist'+num+'" class = "QAindex"><a href=#QA'+num+'>Q. '+qalist[0][num]+'</a></div>'
+    let words = '<div id = "QAlist'+num+'" class = "QAindex whiteblock"><a class="linkwhite" href=#QA'+num+'>Q. '+qalist[0][num]+'</a></div>'
     return words;
 }
 function QAtemplate(num){
     let words1 = '<div id = "Q'+num+'">Q. '+qalist[0][num]+'</div>'
     ,words2 = '<div id = "A'+num+'">A. '+qalist[1][num]+'</div>'
-    ,unit = '<div id = "QA'+num+'"class = "QA">'+words1+words2+'</div>'
+    ,unit = '<div id = "QA'+num+'"class = "QA whiteblock">'+words1+words2+'</div>'
 
     return unit;
 }
