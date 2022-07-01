db17311df3cf7f507aae60b269dc78043bf851cb
Lona <103243217+DoLonaAnd@users.noreply.github.com>
2022-07-01 18:47:17 +0900

    [2022/07/01 18:46] add form,index

--- Sources/script.js
+++ Sources/script.js
@@ -9,6 +9,24 @@ const qalist = [
     ]
 ];
 
+const index = [
+    [
+        ["#index"],
+        ["#QAlist"],
+        ["#form"]
+    ],
+    [
+        ["目次"],
+        ["Q & A"],
+        ["質問フォーム"]
+    ]
+];
+
+//生成テンプレート
+function indextemplate(num){
+    let words = '<div class="index"'+num+'><a href='+index[0][num]+'>'+index[1][num]+'</a></div>';
+    return words;
+}
 function QAindextemplate(num){
     let words = '<div id = "QAlist'+num+'" class = "QAindex"><a href=#QA'+num+'>Q.'+qalist[0][num]+'</a></div>'
     return words;
@@ -21,19 +39,32 @@ function QAtemplate(num){
     return unit;
 }
 
+//生成
+function createIndex(){
+    document.getElementById("index").innerHTML +="<br>";
+    for(let i=0;i<index[1].length;i++){
+        document.getElementById("index").innerHTML += indextemplate(i);
+    }
+    document.getElementById("index").innerHTML +="<br>";
+}
+
 function createQAIndex(){
-    for(var i=0;i<qalist[1].length;i++){
+    for(let i=0;i<qalist[1].length;i++){
         document.getElementById("QAindex").innerHTML += QAindextemplate(i);
     }
+    document.getElementById("QAindex").innerHTML +="<br>";
 }
 
 function createQA(){
-    for(var i=0;i<qalist[1].length;i++){
+    for(let i=0;i<qalist[1].length;i++){
         document.getElementById("QA").innerHTML += QAtemplate(i);
     }
+    document.getElementById("QA").innerHTML +="<br>";
 }
 
+
 window.onload = function(){
+    createIndex();
     createQAIndex();
     createQA();
 }
