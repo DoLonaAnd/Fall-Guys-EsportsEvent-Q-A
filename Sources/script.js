83236a8e9f1d52944bd8f139ea1b604fa942ab1f
Lona <103243217+DoLonaAnd@users.noreply.github.com>
2022-07-01 17:34:02 +0900

    [2022/07/01 17:33]サイトの動的生成化

---
+++ Sources/script.js
@@ -0,0 +1,37 @@
+const qalist = [
+    [
+        ["会場はどこですか？"],
+        ["参加費などはありますか？"]
+    ],
+    [
+        ["近畿大学 Eキャンパス E館1階eスポーツアリーナです。"],
+        ["完全無料です！"]
+    ]
+];
+
+function QAindextemplate(num){
+    let words = '<div id = "QAlist'+num+'"><a href=#QA'+num+'>Q.'+qalist[0][num]+'</a></div>'
+    return words;
+}
+function QAtemplate(num){
+    let words1 = '<div id = "Q'+num+'">'+qalist[0][num]+'</div>'
+    ,words2 = '<div id = "A'+num+'">'+qalist[1][num]+'</div>';
+    return words1 + words2;
+}
+
+function createQAIndex(){
+    for(var i=0;i<qalist[1].length;i++){
+        document.getElementById("QAindex").innerHTML += QAindextemplate(i);
+    }
+}
+
+function createQA(){
+    for(var i=0;i<qalist[1].length;i++){
+        document.getElementById("QA").innerHTML += QAtemplate(i);
+    }
+}
+
+window.onload = function(){
+    createQAIndex();
+    createQA();
+}
